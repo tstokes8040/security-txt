afa1d5b637f66a82112bb8d037bb8d10f05580b8
Tyler Stokes <tyler.stokes@wpengine.com>
2023-06-06 22:19:55 -0500

    Version 1.0.4 patch

--- README.txt
+++ README.txt
@@ -2,8 +2,8 @@
 Plugin Name: Security-TXT
 Tags: security, infosec, netsec, security.txt, disclosure, bug bounty
 Requires at least: 5.0
-Tested up to: 6.1.1
-Stable tag: 6.1.1
+Tested up to: 6.2.2
+Stable tag: 1.0.4
 Requires PHP: 7.0
 License: GPLv3 or later
 License URI: https://github.com/tstokes8040/security-txt/blob/main/LICENSE.txt
@@ -31,10 +31,10 @@ We welcome development to make this plugin even better! Create your [pull reques
 
 == Changelog ==
 
-= 1.0.3 =
+= 1.0.4 =
 * Initial release.
 
 == Upgrade Notice ==
 
-= 1.0.3 =
+= 1.0.4 =
 * Initial release.
--- security-txt.php
+++ security-txt.php
@@ -4,7 +4,7 @@
  * Description: Creates a security.txt file with contact info.
  * Plugin Name: Security-TXT
  * Plugin URI:
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author:  Tyler Stokes
  * Author URI: https://tswebservices.com/
  * License: GPLv3 or later
@@ -37,96 +37,80 @@ global $file_path;
 $file_path = get_home_path() . "security.txt";
 
 //Create or Update our security.txt file
-if ( ! function_exists( 'st_create_update_file' ) ) {
-	function st_create_update_file() {
-        global $file_path;
-		$securityTXTFile = fopen( $file_path, 'w' ) or wp_die( 'Unable to open or create file! Please check permissions.' );
-		$optionSetting   = get_option( 'st_content' );
-		// Check to see if we have an option in the DB. If so, write to the file.
-		if ( $optionSetting ) {
-			fwrite( $securityTXTFile, $optionSetting );
-		}
-		fclose( $securityTXTFile );
+function sdottxt_create_update_file() {
+	global $file_path;
+	$securityTXTFile = fopen( $file_path, 'w' ) or wp_die( 'Unable to open or create file! Please check permissions.' );
+	$optionSetting   = get_option( 'sdottxt_content' );
+	// Check to see if we have an option in the DB. If so, write to the file.
+	if ( $optionSetting ) {
+		fwrite( $securityTXTFile, $optionSetting );
 	}
+	fclose( $securityTXTFile );
 }
 
 //Deactivating the plugin
-if ( ! function_exists( 'st_deactivatePlugin' ) ) {
-    function st_deactivatePlugin() {
-		st_delete_file();
-	}
+function sdottxt_deactivatePlugin() {
+	sdottxt_delete_file();
 }
 
 //Uninstalling the plugin
-if ( ! function_exists( 'st_uninstallPlugin' ) ) {
-	function st_uninstallPlugin() {
-		st_delete_file();
-		st_maybe_delete_options();
-	}
+function sdottxt_uninstallPlugin() {
+	sdottxt_delete_file();
+	sdottxt_maybe_delete_options();
 }
 
 //Delete file
-if ( ! function_exists('st_delete_file') )  {
-	function st_delete_file() {
-		global $file_path;
-		unlink( $file_path );
-	}
+function sdottxt_delete_file() {
+	global $file_path;
+	unlink( $file_path );
 }
 
 //Maybe Delete Options
-if ( ! function_exists('st_maybe_delete_options') ) {
-	function st_maybe_delete_options() {
-		// Check to see if they have checked the delete DB data. If so, then delete DB data.
-		$optionSetting = esc_attr( get_option( 'st_delete_data' ) );
-		if ( 'Yes' === $optionSetting ) {
-			delete_option( 'st_delete_data' );
-			delete_option( 'st_content' );
-		}
+function sdottxt_maybe_delete_options() {
+	// Check to see if they have checked the delete DB data. If so, then delete DB data.
+	$optionSetting = esc_attr( get_option( 'sdottxt_delete_data' ) );
+	if ( 'Yes' === $optionSetting ) {
+		delete_option( 'sdottxt_delete_data' );
+		delete_option( 'sdottxt_content' );
 	}
 }
 
 //Setup Admin Menu Item
-add_action( 'admin_menu', 'st_menu_item' );
-if ( ! function_exists('st_menu_item') ) {
-	function st_menu_item() {
-		add_submenu_page( 'tools.php', 'Security TXT', 'Security TXT', 'manage_options', 'security-txt', 'securitytxt_form_func' );
-		add_action( 'admin_init', 'register_securitytxt_settings' );
-	}
+add_action( 'admin_menu', 'sdottxt_menu_item' );
+function sdottxt_menu_item() {
+	add_submenu_page( 'tools.php', 'Security TXT', 'Security TXT', 'manage_options', 'security-txt', 'securitytxt_form_func' );
+	add_action( 'admin_init', 'register_securitytxt_settings' );
 }
 
 //Register SecurityTXT Settings
-if ( ! function_exists('register_securitytxt_settings') ) {
-	function register_securitytxt_settings() {
-		register_setting( 'security_txt_settings_group', 'st_content' );
-		register_setting( 'security_txt_settings_group', 'st_delete_data' );
-	}
+function register_securitytxt_settings() {
+	register_setting( 'security_txt_settings_group', 'sdottxt_content' );
+	register_setting( 'security_txt_settings_group', 'sdottxt_delete_data' );
 }
 
 //SecurityTXT Form Setup
-if ( ! function_exists('securitytxt_form_func') ) {
-	function securitytxt_form_func() {
-		?>
-		<div class="wrap">
-			<h1>Security.txt Settings</h1>
-			<form method="post" action="options.php">
-				<?php settings_fields( 'security_txt_settings_group' ); ?>
-				<?php do_settings_sections( 'security_txt_settings_group' ); ?>
-				<label for="st_content">Enter your information to display in the security.txt file</label>
-				<br>
-				<textarea rows="6" cols="50" type="text" name="st_content" ><?php echo esc_attr( get_option( 'st_content' ) ); ?></textarea>
-				<br>
-				<label for="delete_data">Delete Data on uninstall?</label>
-				<br>
-				<input type="radio" name="st_delete_data" class="" value="Yes" <?php checked( 'Yes', get_option( 'st_delete_data' ) ); ?>/>Yes
-				<input type="radio" name="st_delete_data" class="" value="No" <?php checked( 'No', get_option( 'st_delete_data' ) ); ?>/>No
-				<?php submit_button(); ?>
-			</form>
-		</div>
-		<?php
-	}
+function securitytxt_form_func() {
+	?>
+	<div class="wrap">
+		<h1>Security.txt Settings</h1>
+		<form method="post" action="options.php">
+			<?php settings_fields( 'security_txt_settings_group' ); ?>
+			<?php do_settings_sections( 'security_txt_settings_group' ); ?>
+			<label for="sdottxt_content">Enter your information to display in the security.txt file</label>
+			<br>
+			<textarea rows="6" cols="50" type="text" name="sdottxt_content" ><?php echo esc_attr( get_option( 'sdottxt_content' ) ); ?></textarea>
+			<br>
+			<label for="delete_data">Delete Data on uninstall?</label>
+			<br>
+			<input type="radio" name="sdottxt_delete_data" class="" value="Yes" <?php checked( 'Yes', get_option( 'sdottxt_delete_data' ) ); ?>/>Yes
+			<input type="radio" name="sdottxt_delete_data" class="" value="No" <?php checked( 'No', get_option( 'sdottxt_delete_data' ) ); ?>/>No
+			<?php submit_button(); ?>
+		</form>
+	</div>
+	<?php
 }
 
-register_activation_hook( __FILE__, 'st_create_update_file' );
-register_deactivation_hook( __FILE__, 'st_deactivatePlugin' );
-register_uninstall_hook( __FILE__, 'st_uninstallPlugin' );
-add_action('update_option_st_content', 'st_create_update_file');
+register_activation_hook( __FILE__, 'sdottxt_create_update_file' );
+register_deactivation_hook( __FILE__, 'sdottxt_deactivatePlugin' );
+register_uninstall_hook( __FILE__, 'sdottxt_uninstallPlugin' );
+add_action('update_option_sdottxt_content', 'sdottxt_create_update_file');
